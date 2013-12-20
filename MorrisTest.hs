a26b1528fd8c2867117dd982b53133a8b0127213
Heff <rasmus.egero@gmail.com>
2013-12-21 00:53:19 +0100

    Generator for arbitrary Boards

--- MorrisTest.hs
+++ MorrisTest.hs
@@ -8,6 +8,12 @@ instance Arbitrary Pos where
 
 instance Arbitrary Player where
   arbitrary = oneof [return $ Player White, return $ Player Black]
+  
+instance Arbitrary Color where
+  arbitrary = oneof [return White, return Black]
 
+-- As the last bits of the Board are not currently in use, this will do:
 instance Arbitrary Board where
-  arbitrary = return blankBoard
+   arbitrary = 
+      do board <- arbitrary
+         return (Board board)
